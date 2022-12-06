e43d5c569f0503ea09238328db5d1b2a2552b94b
Fryderyk Róg <s180265@student.pg.edu.pl>
2022-12-06 12:01:52 +0100

    added init version of anti-character-swap filter

--- src/filters.py
+++ src/filters.py
@@ -1,9 +1,32 @@
+import yaml
 """
 Module containing all micro filers for word validation
 """
 
+CHAR_SWAPS_FILEPATH = "../data/char_swaps.yaml"
 
+
+def charswap_filter(word: str):
+    def swap_words(s, x, y):
+        return y.join(part.replace(y, x) for part in s.split(x))
+            
+    with open(CHAR_SWAPS_FILEPATH, "r", encoding="utf8") as stream:
+        possible_swaps = yaml.safe_load(stream)["swaps_v2"]
+    
+    # filtering out capitalized letters     
+    lowercase = word.lower()
+    
+    for letter, combinations in possible_swaps.items():
+        for combination in combinations:
+            if combination in lowercase:
+                lowercase = swap_words(lowercase, combination, letter)
+
+    print(lowercase)
+                
 def example_filer(word: str):
     if word == "kurva":
         return False
     return True
+
+if __name__ == "__main__":
+    charswap_filter("Koorwa")
