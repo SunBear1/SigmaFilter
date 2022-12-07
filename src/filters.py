8d852447c13b9af473a4600e39d160f73ef6e345
Fryderyk Róg <s180265@student.pg.edu.pl>
2022-12-07 18:37:39 +0100

    fixed combination appendment to temp list

--- src/filters.py
+++ src/filters.py
@@ -7,33 +7,29 @@ Module containing all micro filers for word validation
 def charswap_filter(word: str) -> list:
             
     #STEP 1 - initial replacements with reference to Leet system. Capitalized letters do not matter in this filter
-    lowercase = word.lower()
-    
     for combination, letter in SourceDictionaries.POSSIBLE_SWAPS.items():
-        if combination in lowercase:
-            lowercase = lowercase.replace(combination, letter)
+        if combination in word:
+            word = word.replace(combination, letter)
     
+    # 
     possible_profanities = list()
-    possible_profanities.append(lowercase)
-    #STEP 2 - ambiguity check. f.e '4' can mean both 'a' or 'h' according to Leet so 2 variations are created instead of 1.
+    possible_profanities.append(word)
+    
+    # STEP 2 - ambiguity check. f.e '4' can mean both 'a' or 'h' according to Leet so 2 variations are created instead of 1.
     for ambiguity, letters in SourceDictionaries.AMBIGUITIES.items():
-        if ambiguity in lowercase:
+        if ambiguity in word:
             for letter in letters:
                 temp = list()
                 for profanity in possible_profanities:
                     if ambiguity in profanity:
                         if profanity.count(ambiguity) > 1:
+                            # Combinations of ambiguous letter replacements. F.e "iloi" -> ["lloi","llol","ilol"]
                             options = [(c,) if c != ambiguity else (ambiguity, letter) for c in profanity]
-                            temp = [''.join(o) for o in product(*options)]
-                            temp = temp[1:]
+                            temp.extend([''.join(o) for o in product(*options)])
                         else: 
                             temp.append(profanity.replace(ambiguity, letter))
                 possible_profanities.extend(temp)
     
-    #STEP 3 - verifying if there is a profanity in possible_profanities list
-    # for profanity in possible_profanities:
-    #     if profanity in SourceDictionaries.RAW_BAD_WORDS:
-    #         return False
     return possible_profanities
 
 
@@ -41,3 +37,6 @@ def example_filer(word: str):
     if word == "kurva":
         return False
     return True
+
+if __name__ == "__main__":
+    print(charswap_filter("plndol"))
