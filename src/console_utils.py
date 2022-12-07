16f32c0510a3d0e7e00c36b4dcd2804053d43065
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-12-07 20:36:21 +0100

    review v1

--- src/console_utils.py
+++ src/console_utils.py
@@ -1,6 +1,8 @@
 """
 Module containing console UI functions
 """
+from src.filters import remove_spaces, remove_repeats, charswap_filter, remove_endings
+from src.source_dicts import SourceDictionaries
 
 
 def load_input_from_file(path: str) -> list:
@@ -10,3 +12,31 @@ def load_input_from_file(path: str) -> list:
             for word in line.split():
                 words.append(word)
     return words
+
+
+def filter_badwords(text: list) -> list:
+    for input_word in text:
+        no_repeats = remove_repeats(word=input_word)
+        for word in charswap_filter(word=no_repeats):
+            no_endings = remove_endings(word)
+            if no_endings in SourceDictionaries.RAW_BAD_WORDS:
+                idx = text.index(input_word)
+                text[idx] = input_word.replace(input_word, ("*" * len(input_word)))
+    return text
+
+
+def filter_badwords_adjacent_words(text: list) -> list:
+    i = 0
+    while True:
+        if i + 1 >= len(text):
+            break
+        no_spaces = remove_spaces(first_word=text[i], second_word=text[i + 1])
+        no_repeats = remove_repeats(word=no_spaces)
+        for word in charswap_filter(word=no_repeats):
+            no_endings = remove_endings(word)
+            if no_endings in SourceDictionaries.RAW_BAD_WORDS:
+                text[i] = str(text[i] + text[i + 1])
+                text[i] = text[i].replace(text[i], ("*" * len(text[i])))
+                text.pop(i + 1)
+        i += 1
+        return text
--- src/main.py
+++ src/main.py
@@ -1,47 +1,29 @@
 """
 Main project module
 """
-from console_utils import load_input_from_file
-from src.filters import remove_spaces, remove_repeats, charswap_filter, remove_endings
-from src.source_dicts import SourceDictionaries
+from console_utils import load_input_from_file, filter_badwords, filter_badwords_adjacent_words
 
 INPUT_FILE_PATH = "../wejscie.txt"
 
 if __name__ == '__main__':
-    option = input("1) Wczytaj tekst z pliku\n2) Wczytaj tekst z konsoli\n")
-    if option == "1":
-        text_to_filter = load_input_from_file(path=INPUT_FILE_PATH)
-    elif option == "2":
-        text_to_filter = input("Wprowadź tekst:\n")
-    else:
-        option = input("Nieprawidłowa opcja\n1) Wczytaj tekst z pliku\n2) Wczytaj tekst z konsoli\n")
+
+    good_input = False
+    while not good_input:
+        option = input("1) Wczytaj tekst z pliku\n2) Wczytaj tekst z konsoli\n")
+        if option == "1":
+            text_to_filter = load_input_from_file(path=INPUT_FILE_PATH)
+            good_input = True
+        elif option == "2":
+            text_to_filter = input("Wprowadź tekst:\n").split()
+            good_input = True
 
     for i in range(len(text_to_filter)):
         text_to_filter[i] = text_to_filter[i].lower()
 
     # Filter words without space filtering
-    for input_word in text_to_filter:
-        no_repeats = remove_repeats(word=input_word)
-        for word in charswap_filter(word=no_repeats):
-            no_endings = remove_endings(word)
-            if no_endings in SourceDictionaries.RAW_BAD_WORDS:
-                idx = text_to_filter.index(input_word)
-                text_to_filter[idx] = input_word.replace(input_word, ("*" * len(input_word)))
-
-    print(text_to_filter)
+    filter_badwords_results = filter_badwords(text=text_to_filter)
+    print(filter_badwords_results)
 
     # Filter words with space filtering
-    i = 0
-    while True:
-        if i + 1 >= len(text_to_filter):
-            break
-        no_spaces = remove_spaces(first_word=text_to_filter[i], second_word=text_to_filter[i + 1])
-        no_repeats = remove_repeats(word=no_spaces)
-        for word in charswap_filter(word=no_repeats):
-            no_endings = remove_endings(word)
-            if no_endings in SourceDictionaries.RAW_BAD_WORDS:
-                text_to_filter[i] = str(text_to_filter[i] + text_to_filter[i + 1])
-                text_to_filter[i] = text_to_filter[i].replace(text_to_filter[i], ("*" * len(text_to_filter[i])))
-                text_to_filter.pop(i + 1)
-        i += 1
-    print(text_to_filter)
+    filter_badwords_adjacent_words_results = filter_badwords_adjacent_words(text=text_to_filter)
+    print(filter_badwords_adjacent_words_results)
