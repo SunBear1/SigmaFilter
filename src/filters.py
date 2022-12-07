6d2448b77b03f04e9460c00379c490f964b9f0d9
Fryderyk Róg <s180265@student.pg.edu.pl>
2022-12-07 17:34:30 +0100

    upgraded ambiguity and refactored source dict a little bit

--- src/filters.py
+++ src/filters.py
@@ -1,39 +1,42 @@
-import yaml
+from source_dicts import SourceDictionaries
+from itertools import product
 """
 Module containing all micro filers for word validation
 """
 
-CHAR_SWAPS_FILEPATH = "../data/char_swaps.yaml"
-
-
 def charswap_filter(word: str):
             
-    with open(CHAR_SWAPS_FILEPATH, "r", encoding="utf8") as stream:
-        yml_data = yaml.safe_load(stream)
-    possible_swaps = yml_data["swaps"]
-    ambiguities = yml_data["ambiguities"]
-    
-    #STEP 1 - initial replacements with reference to Leet system. 
+    #STEP 1 - initial replacements with reference to Leet system. Capitalized letters do not matter in this filter
     lowercase = word.lower()
-    for letter, combinations in possible_swaps.items():
-        for combination in combinations:
-            if combination in lowercase:
-                lowercase = lowercase.replace(combination, letter)
     
-    possible_profanities = []
+    for combination, letter in SourceDictionaries.POSSIBLE_SWAPS.items():
+        if combination in lowercase:
+            lowercase = lowercase.replace(combination, letter)
+    
+    possible_profanities = list()
+    possible_profanities.append(lowercase)
     #STEP 2 - ambiguity check. f.e '4' can mean both 'a' or 'h' according to Leet so 2 variations are created instead of 1.
-    for ambiguity, letters in ambiguities.items():
-        for letter in letters:
-            if ambiguity in lowercase:
-                possible_profanities.append(lowercase.replace(combination, letter))
+    for ambiguity, letters in SourceDictionaries.AMBIGUITIES.items():
+        if ambiguity in lowercase:
+            for letter in letters:
+                temp = list()
+                for profanity in possible_profanities:
+                    if ambiguity in profanity:
+                        temp.append(profanity.replace(ambiguity, letter))
+                possible_profanities.extend(temp)
     
-    
+    #STEP 3 - verifying if there is a profanity in possible_profanities list
+    # for profanity in possible_profanities:
+    #     if profanity in SourceDictionaries.RAW_BAD_WORDS:
+    #         return False
+    return possible_profanities
 
-                
+
 def example_filer(word: str):
     if word == "kurva":
         return False
     return True
 
 if __name__ == "__main__":
-    charswap_filter("Koorw4")
+    print(charswap_filter("kuЯvvy"))
+    #print(charswap_filter("|O!/\/ |>0I")
