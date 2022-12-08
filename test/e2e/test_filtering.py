06134f8693518b8d646e2282312d599442fbbf41
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-12-08 20:33:33 +0100

    more test words

--- test/e2e/test_filtering.py
+++ test/e2e/test_filtering.py
@@ -26,6 +26,22 @@ from src.console_utils import filter_badwords, filter_badwords_adjacent_words
     ("zdzria", 6),
     ("dzz1r@", 6),
     ("zzddzziirraa", 6),
+    ("c1pa", 4),
+    ("c1111p4", 4),
+    ("c1p4", 4),
+    ("c11.p4", 4),
+    ("kurw.a", 4),
+    ("kur w a", 4),
+    ("c1pa", 4),
+    ("c1pa", 4),
+    ("kurvv44", 5),
+    ("g0wnÓ", 5),
+    ("g0wnÓ", 5),
+    ("jp", 2),
+    ("pj", 2),
+    ("j|°", 2),
+    ("j]p", 2),
+    ("jjjp", 2),
 ])
 def test_single_words(test_word, expected):
 
