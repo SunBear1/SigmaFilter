0b742766535a3141165c0779a53ffdba6410a4e3
grepoz <grzegorz.pozorski@gmail.com>
2022-12-08 21:42:39 +0100

    added simple tests

--- test/unit/test_filters.py
+++ test/unit/test_filters.py
@@ -1,6 +1,14 @@
 from src.console_utils import filter_badwords, filter_badwords_adjacent_words
-from src.filters import remove_repeats
+from src.filters import remove_repeats, remove_endings
 
 
 def test_remove_repeats():
     assert remove_repeats("kurrrwwwaaa") == "kurwa"
+
+
+def test_removingEnding_whenWordIsNoun_thenReturnsWordCore():
+    assert remove_endings("kurwę") == "kurwa"
+
+
+def test_removingEnding_whenWordIsAdjective_thenReturnsWordCore():
+    assert remove_endings("jebana") == "jebać"
