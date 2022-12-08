47e08d2fa1c0a10df50d98ee924bfd7fad117bd3
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-12-08 16:10:08 +0100

    code reformat by Black, some CLI cosmetics and fixes

--- src/filters.py
+++ src/filters.py
@@ -7,28 +7,27 @@ from source_dicts import SourceDictionaries
 
 
 def charswap_filter(word: str) -> list:
-
     # STEP 1 - initial replacements with reference to Leet system. Capitalized letters do not matter in this filter
     for combination, letter in SourceDictionaries.POSSIBLE_SWAPS.items():
         if combination in word:
             word = word.replace(combination, letter)
-    
+
     possible_profanities = [word]
-    
+
     # STEP 2 - ambiguity check. f.e '4' can mean both 'a' or 'h' according to Leet so 2 variations are created instead of 1.
     for ambiguity, letters in SourceDictionaries.AMBIGUITIES.items():
         if ambiguity in word:
             for letter in letters:
                 temp = list()
                 for profanity in possible_profanities:
-                        if profanity.count(ambiguity) > 1:
-                            # Combinations of ambiguous letter replacements. F.e "iloi" -> ["lloi","llol","ilol"]
-                            options = [(c,) if c != ambiguity else (ambiguity, letter) for c in profanity]
-                            temp.extend([''.join(o) for o in product(*options)])
-                        elif profanity.count(ambiguity) == 1:
-                            temp.append(profanity.replace(ambiguity, letter))
+                    if profanity.count(ambiguity) > 1:
+                        # Combinations of ambiguous letter replacements. F.e "iloi" -> ["lloi","llol","ilol"]
+                        options = [(c,) if c != ambiguity else (ambiguity, letter) for c in profanity]
+                        temp.extend([''.join(o) for o in product(*options)])
+                    elif profanity.count(ambiguity) == 1:
+                        temp.append(profanity.replace(ambiguity, letter))
                 possible_profanities.extend(temp)
-                
+
     return possible_profanities
 
 
@@ -41,7 +40,7 @@ def remove_repeats(word: str) -> str:
     i = 0
     max_index = len(chars) - 2
     while i <= max_index:
-        if chars[i] == chars[i+1]:
+        if chars[i] == chars[i + 1]:
             chars.pop(i)
             max_index -= 1
         else:
