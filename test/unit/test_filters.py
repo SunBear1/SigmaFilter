0dfa22d0476935ef36d0c6d2edee869af4fc9c38
grepoz <grzegorz.pozorski@gmail.com>
2022-12-11 11:49:54 +0100

    use pytest instead of unittest

--- test/unit/test_filters.py
+++ test/unit/test_filters.py
@@ -1,19 +1,25 @@
+import pytest
+
 from filters import remove_repeats, remove_endings
-import unittest
 import morfeusz2
 
 
-class TestSum(unittest.TestCase):
-    morf = morfeusz2.Morfeusz()
+@pytest.fixture
+def analyzer():
+    return morfeusz2.Morfeusz()
 
-    def test_remove_repeats(self):
-        self.assertEqual("kurwa", remove_repeats("kurrrwwwaaa"))
 
-    def test_removingEnding_whenWordIsNoun_thenReturnsWordCore(self):
-        self.assertEqual("kurwa", remove_endings(self.morf, "kurwę"))
+def test_remove_repeats():
+    assert "kurwa" == remove_repeats("kurrrwwwaaa")
 
-    def test_removingEnding_whenWordIsAdjective_thenReturnsWordCore(self):
-        self.assertEqual("jebać", remove_endings(self.morf, "jebana"))
 
-    def test_removingEnding_whenWordIsAdverb_thenReturnsWordCore(self):
-        self.assertEqual("chujowy", remove_endings(self.morf, "chujowa"))
+def test_removingEnding_whenWordIsNoun_thenReturnsWordCore(analyzer):
+    assert "kurwa" == remove_endings(analyzer, "kurwę")
+
+
+def test_removingEnding_whenWordIsAdjective_thenReturnsWordCore(analyzer):
+    assert "jebać" == remove_endings(analyzer, "jebana")
+
+
+def test_removingEnding_whenWordIsAdverb_thenReturnsWordCore(analyzer):
+        assert "chujowy" == remove_endings(analyzer, "chujowa")
